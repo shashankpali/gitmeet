0f63ae29c02303f4d4629dee85d0cbf95b4722d9
Shashank Pali <shank.pali@gmail.com>
2022-08-02 15:46:09 +0530

    added constants file

---
+++ GitMeet/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constant.swift
+//  GitMeet
+//
+//  Created by Shashank Pali on 02/08/22.
+//
+
+import Foundation
+
+struct Constants {
+    static let baseUrl = "https://api.github.com/repos/"
+    static let endPoint = "/pulls?state=all"
+    
+    struct Errors {
+        static let invalidInput = "either username or reponame is incorrect."
+        static let emptyRecord = "There is no record for %@. Please try later or some other repo"
+        static let generic = "Somthing went wrong"
+    }
+}
