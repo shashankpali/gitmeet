1a7dce4b41731ffcd785d3b1ef1983298ad18205
Shashank Pali <shank.pali@gmail.com>
2022-08-02 17:00:57 +0530

    failure coverage

--- GitMeet/Helpers/Network.swift
+++ GitMeet/Helpers/Network.swift
@@ -13,24 +13,26 @@ enum NetworkResult<T, String> {
 
 struct Network {
     
-    static func getData<T: Codable>(urlString: String, type: T.Type, callback:@escaping (_ res: NetworkResult<[T], String>) -> Void) {
+    func getData<T: Codable>(urlString: String, type: T.Type, callback:@escaping (_ res: NetworkResult<[T]?, String>) -> Void) {
         guard let url = URL(string: urlString) else {return}
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = "GET"
         
         URLSession.shared.dataTask(with: urlRequest) { data, response, error in
             
-            guard let httpResponse = response as? HTTPURLResponse else {return callback(.failure(error?.localizedDescription ?? ""))}
+            guard let httpResponse = response as? HTTPURLResponse else {return callback(.failure(error?.localizedDescription ?? Constants.Errors.generic))}
             
             if 200...299 ~= httpResponse.statusCode {
                 do {
                     let parsedData = try JSONDecoder().decode([T].self, from: data!)
                     callback(.success(parsedData))
                 } catch {
-                    callback(.failure(""))
+                    callback(.failure(error.localizedDescription ))
                 }
+            }else if 400...499 ~= httpResponse.statusCode {
+                callback(.failure(error?.localizedDescription ?? Constants.Errors.invalidInput))
             }else {
-                callback(.failure(""))
+                callback(.failure(error?.localizedDescription ?? Constants.Errors.generic))
             }
             
         }.resume()
